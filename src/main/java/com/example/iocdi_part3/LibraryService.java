c6fb82370e63023b572260c60bf508f388e52acb
G00394619@atu.ie <g00394619@atu.ie>
2023-10-12 17:57:32 +0100

    Added lists declaration and annotation

--- src/main/java/com/example/iocdi_part3/LibraryService.java
+++ src/main/java/com/example/iocdi_part3/LibraryService.java
@@ -1,7 +1,12 @@
 package com.example.iocdi_part3;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import java.util.ArrayList;
 
 @Service
 public class LibraryService {
+    private List<Book> library = new ArrayList<>();
 }
