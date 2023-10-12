b918dbb97f683e71672375593d96f938203e3cd7
G00394619@atu.ie <g00394619@atu.ie>
2023-10-12 17:41:05 +0100

    Added Dependencies and Annotations

--- src/main/java/com/example/iocdi_part3/Book.java
+++ src/main/java/com/example/iocdi_part3/Book.java
@@ -1,4 +1,12 @@
 package com.example.iocdi_part3;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
 public class Book {
 }
