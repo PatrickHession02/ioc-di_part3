45d7adc2858ab5ff1b158c66f6ab2c5331941bcb
G00394619@atu.ie <g00394619@atu.ie>
2023-10-12 18:19:24 +0100

    Added postmapping + response status

--- src/main/java/com/example/iocdi_part3/LibraryController.java
+++ src/main/java/com/example/iocdi_part3/LibraryController.java
@@ -1,7 +1,22 @@
 package com.example.iocdi_part3;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class LibraryController {
+    private LibraryService libraryService;
+
+    @Autowired
+
+    public LibraryController(LibraryService libraryService)
+    {
+      this.libraryService = libraryService;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/AddBook")
 }
