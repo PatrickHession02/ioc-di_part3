7d840a767ee1fb3ef35220f2ffba4f3630258996
G00394619@atu.ie <g00394619@atu.ie>
2023-10-12 17:36:13 +0100

    Initial commit

---
+++ src/main/java/com/example/iocdi_part3/IocDiPart3Application.java
@@ -0,0 +1,13 @@
+package com.example.iocdi_part3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IocDiPart3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IocDiPart3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/iocdi_part3/IocDiPart3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iocdi_part3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IocDiPart3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
